a8feb5d2bee157b737b5f2b4ed0a932a06762653
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-16 20:42:15 +0200

    feat: show feedback from deleting

--- GoalsApp/components/GoalItem.jsx
+++ GoalsApp/components/GoalItem.jsx
@@ -1,14 +1,18 @@
 import { StyleSheet, Text, View, Pressable } from "react-native";
 
-const GoalItem = (props) => {
+const GoalItem = props => {
   return (
-    <Pressable onPress={props.onDeleteItem}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#dddddd" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({pressed}) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>
           {props.text}
         </Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -17,11 +21,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc"
   },
+  pressedItem: {
+    opacity: 0.5
+  },
   goalText: {
+    padding: 8,
     color: "#fff"
   }
 });
